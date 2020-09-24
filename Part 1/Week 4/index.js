bc3066658bb43986a8b51598498834b066941f4b
Artur <art.yumaev@gmail.com>
2020-09-24 23:14:26 +0300

    added week 4

--- Part 1/Week 4/index.js
+++ Part 1/Week 4/index.js
@@ -1,91 +1,48 @@
-var friends = [
-    {
-        name: 'Сэм',
-        gender: 'Мужской',
-        email: 'luisazamora@example.com',
-        favoriteFruit: 'Картофель'
-    },
-	{
-        name: 'Саша',
-        gender: 'Мужской',
-        email: 'luisazamora@example.com',
-        favoriteFruit: 'Картофель'
-    },
-	{
-        name: 'Алекс',
-        gender: 'Мужской',
-        email: 'luisazamora@example.com',
-        favoriteFruit: 'Морковь'
-    },
-    {
-        name: 'Эмили',
-        gender: 'Женский',
-        email: 'roachpugh@example.com',
-        favoriteFruit: 'Яблоко'
-    },
-    {
-        weight: '16'
-    }
-];
-
-/**
- * @param {Array} collection
- * @params {Function[]} – Функции для запроса
- * @returns {Array}
- */
 function query() {
-	console.log(arguments)
+  var data = [...arguments['0']]
+  var keys = Object.keys(arguments).filter(k => k != '0')
+
+  for (var k of keys) {
+    if (arguments[k][0] == 'filterIn')
+      data = f(data, arguments[k][1], arguments[k][2])
+  }
+
+  for (var k of keys) {
+    if (arguments[k][0] == 'select')
+      data = s(data, Object.keys(arguments[k][1]).map(ind => arguments[k][1][ind]))
+  }
+
+  return data
 }
 
-/**
- * @params {String[]}
- */
 function select() {
-	return ['select', arguments];
+  return ['select', arguments];
 }
 
-/**
- * @param {String} property – Свойство для фильтрации
- * @param {Array} values – Массив разрешённых значений
- */
 function filterIn(property, ...values) {
-	return ['filterIn', property, ...values];
+  return ['filterIn', property, ...values];
 }
-
-module.exports = {
-    query: query,
-    select: select,
-    filterIn: filterIn
-};
-
-var bestFriends = query(
-  friends,
-  select('name', 'gender', 'email'),
-  filterIn('favoriteFruit', ['Яблоко', 'Картофель'])
-);
 
 const s = (data, cols) => (
-    [...data].map(obj => (
-        Object
-            .keys(obj)
-            .filter(key => cols.includes(key))
-            .reduce((prev, k) => {
-                prev[k] = obj[k]
-                return prev
-            }, {})
-    )).filter(obj => Object.keys(obj).length)
+  [...data].map(obj => (
+    Object
+      .keys(obj)
+      .filter(key => cols.includes(key))
+      .reduce((prev, k) => {
+        prev[k] = obj[k]
+        return prev
+      }, {})
+  )).filter(obj => Object.keys(obj).length)
 )
 
 const f = (data, field, values) => (
-	[...data].filter(
-		obj => obj[field] && values.includes(obj[field])
-	)
+  [...data].filter(
+    obj => obj[field] && values.includes(obj[field])
+  )
 )
 
-
-/*
-[
-  { name: 'Сэм', gender: 'Мужской', email: 'luisazamora@example.com' },
-  { name: 'Эмили', gender: 'Женский', email: 'roachpugh@example.com' }
-]
-*/
+module.exports = {
+  query: query,
+  select: select,
+  filterIn: filterIn
+}
